215690de008882de86e0eb98b248911f66448cdd
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-08-06 16:03:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 region = "${var.region}"
 #shared_credentials_file = "~/.aws/credentials"
   
-  AWS_ACCESS_KEY_ID = credentials('aws crede')
- AWS_SECRET_ACCESS_KEY = credentials('aws crede')
+  AWS_ACCESS_KEY_ID = credentials("aws crede")
+ AWS_SECRET_ACCESS_KEY = credentials("aws crede")
 
 }
 
