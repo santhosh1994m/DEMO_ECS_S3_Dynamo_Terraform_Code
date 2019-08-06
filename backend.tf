51a1076be83e1182858167611e1126e2dab7d7ca
santhosh1994m <45985363+santhosh1994m@users.noreply.github.com>
2019-08-06 17:11:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,12 @@
 
+terraform {
+backend "s3" {
+bucket = "santhosh-file"
+dynamodb_table = "terraform-state-lock-dynamo"
+key    = "terraform-aws/terraform.tfstate"
+region = "us-west-2"
+encrypt = true
+
+}
+
+}
